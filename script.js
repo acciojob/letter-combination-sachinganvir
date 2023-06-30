322db65f7280b865d574a8bd7bc54856d21deea5
sachinganvir <120235946+sachinganvir@users.noreply.github.com>
2023-06-30 16:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function letterCombinations(input_digit) {
   //Complete the function
+// st    
+	let table={
+		"2": "abc",
+		"3": "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7": "pqrs",
+		"8": "tuv",
+		"9": "wxyz"
+	}
+ 
+	let solution=[];
+ 
+	function solve(currentIndex, calculatedString){
+		if(input.length<= currentIndex){
+			solution.push(calculatedString);
+			return;
+		}
+ 
+		let currentChar = input[currentIndex];
+		for(let i=0;i< table[currentChar].length;i++){
+			solve(currentIndex +1, calculatedString + table[currentChar][i]);
+		}
+	}
+	solve(0, "");
+ 
+	return(solution);
+
+	// se
+	
 }
 
 module.exports = letterCombinations;
